328ffc16f3c104b6409f25eb8735c099da98a343
Norbert Kuchenmeister <norbert.kuchenmeister@ideas.com>
2015-01-27 15:55:24 -0600

    added support for angularjs

--- src/main/java/net/icoachtennis/rosterController.java
+++ src/main/java/net/icoachtennis/rosterController.java
@@ -10,6 +10,6 @@ public class rosterController {
     @RequestMapping("/roster")
     public String allPlayers(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return"roster";
+        return"index";
     }
 }
